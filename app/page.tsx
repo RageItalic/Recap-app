711d2e5d4c0a59dd7d1cee590a8257c7460e6744
Parth Patel <parthpatelgee@gmail.com>
2024-03-02 03:19:36 -0500

    basic home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
+'use client'
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -91,16 +101,34 @@ export default function Home() {
 
 
     <section className="bg-white flex h-screen w-screen">
-      <div className="h-screen flex-initial w-5/12 bg-cover bg-[url('https://images.unsplash.com/photo-1687273705305-d8ff89951fdc?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+      <div className="h-screen flex-initial w-5/12 bg-cover bg-[url(https://images.unsplash.com/photo-1687273705305-d8ff89951fdc?q=80&w=2454&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]">
         <h1 className="p-6 text-4xl text-white">Recap</h1>
       </div>
       <div className="h-screen flex-initial w-7/12">
         <nav className="p-2 flex flex-row w-full justify-between">
           <p>logo</p>
-          <p>menu</p>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>Menu</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Get Started</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>FAQ</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>About</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Contact</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
         </nav>
         <section className="mt-12 pl-2 pr-2 ml-32 mr-9">
           <h1 className="text-4xl uppercase">Share authentic experiences of the places you've visited with the world. Be it next door, or across the ocean.</h1>
+          <div className="pt-4">
+            <Link href={"/login"}>
+              <Button variant={"customGreen"}>Get Started</Button>
+            </Link>
+          </div>
         </section>
       </div>
     </section>
