c82c6cd6862f488726c945c705e3330e596433fa
Taksh13 <ptaksh13@gmail.com>
2024-03-03 01:01:18 -0500

    auth done

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,21 +1,25 @@
-'use client';
+'use client'
 import React from 'react';
 import Map from '@/components/ui/map';
 import SideNav from '@/components/ui/sidenav';
 import RecapCard from '@/components/ui/recapCard';
+import { auth } from '@/auth';
 
-const MyMapPage: React.FC = () => {
+const MyMapPage: React.FC = async () => {
     const first = { lat: 40.715894542723625, lng: -73.99163295995301 };
     const second = { lat: 40.681964285254374, lng: -74.00037204646198 };
     const zoom = 12.5;
 
+    const session = await auth();
+    const user = session?.user;
+    console.log(user);
     return (
         <div className='flex flex-row h-screen'>
             <div className='flex justify-center w-7/12'>
                 <RecapCard />
             </div>
             <div className='flex bg-emerald-500 w-5/12'>
-                <Map apiKey="AIzaSyDlIHh1Ww10Qdt20VJF3k4F-xojzHPZgfY" center={first} locations={[first, second]} zoom={zoom} />
+                <Map apiKey={process.env.GOOGLE_MAPS_API_KEY!} center={first} locations={[first, second]} zoom={zoom} />
             </div>
         </div>
     );
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,7 @@
 
 
-// export default function Page() {
-//     return (
-//         <h1>Login page</h1>
-//     )
-// }
-
-import { signIn } from 'next-auth/react';
-
-const LoginPage = () => {
-    const handleLogin = async () => {
-        await signIn('google'); // Use the provider ID configured in your next-auth configuration
-    };
-
+export default function Page() {
     return (
-        <div>
-            <h1>Login</h1>
-            <button onClick={handleLogin}>Login with Google</button>
-        </div>
-    );
-};
-
-export default LoginPage;
+        <h1>Login page</h1>
+    )
+}
