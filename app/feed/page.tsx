076f6a867ebf65cb8812c0e4a6ca0e8a47481fb4
Parth Patel <parthpatelgee@gmail.com>
2024-03-23 15:49:52 -0400

    quick empty feed handling

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -43,9 +43,12 @@ export default async function Page() {
     return (
         <div className='flex flex-row h-screen'>
             <div className='flex flex-col w-7/12 overflow-y-scroll'>
-                {loggedInUserFeed.map((recap) => (
-                    <RecapCard recap={recap as Recap} key={recap.id} loggedInUser={loggedInUser} />
-                ))}
+                {loggedInUserFeed.length > 0
+                    ? loggedInUserFeed.map((recap) => (
+                        <RecapCard recap={recap as Recap} key={recap.id} loggedInUser={loggedInUser} />
+                    ))
+                    : <p>Your feed is empty! Try following some people first! TAKSH WE FORGOT ABOUT BUILDING THE FUNCTIONALITY TO ALLOW PEOPLE TO FOLLOW EACH OTHER!</p>
+                }
             </div>
             <div className='flex w-5/12'>
                 <Map apiKey={process.env.GOOGLE_MAPS_API_KEY!} center={center} locations={locationCoordsFromFirstRecap} zoom={zoom} />
